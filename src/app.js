3aaeafbf6afc6b0b9f3720ec667011be46639edd
GabrielBragaa <gbragamoura@hotmail.com>
2023-06-25 12:14:20 -0300

    servidor mostra os tweets feitos na tela

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { type } from 'os';
 
 const app = express();
 app.use(cors());
@@ -8,23 +9,54 @@ app.use(express.json());
 const users = [];
 const tweets = [];
 
+let userGlobal = {
+    username: '',
+    avatar: ''
+}
+
+let tweetGlobal = {
+    username: '',
+    tweet: ''
+}
+
 app.post('/sign-up', (req, res) => {
-    users.push({username: req.body.username, avatar: req.body.avatar});
-    res.send('OK');
+    const {username, avatar} = req.body
+    if (username === '' || avatar === '' || typeof(username) !== 'string' || typeof(avatar) !== 'string') {
+        return res.status(400).send('Preencha todos os campos.');
+    }
+
+    userGlobal = {
+        username: username,
+        avatar: avatar
+    }
+
+    users.push(userGlobal);
+    res.status(201).send('OK');
 });
 
+
 app.post('/tweets', (req, res) => {
-    if (!users.includes(req.body.username)) {
-        console.log('UNAUTHORIZED');
-        console.log(users)
-    } else {
-        tweets.push({username: req.body.username, tweet: req.body.tweet});
-        res.send('OK');
+    const {username, tweet} = req.body;
+    tweetGlobal = {
+        username: username,
+        tweet: tweet,
+        avatar: userGlobal.avatar
     }
+
+    if (username === '' || tweet === '' || typeof(username) !== 'string' || typeof(tweet) !== 'string') {
+        return res.status(400).send('Preencha todos os campos.');
+    };
+
+    if (userGlobal.username === undefined || userGlobal.username === '') {
+        return res.status(401).send('UNAUTHORIZED');
+    };
+
+    tweets.push(tweetGlobal);
+    res.status(201).send('OK');
+
 })
 
 app.get('/tweets', (req, res) => {
-
     res.send(tweets);
 })
 
